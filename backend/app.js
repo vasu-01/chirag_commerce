e354ed3ae7b7b7b0a43a486032085cc02b15b71f
Vishal Kumar <vishalyadav6jd@gmail.com>
2025-08-17 12:35:47 +0530

    app update

--- backend/app.js
+++ backend/app.js
@@ -17,9 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "path-to-build-folder", "index.html"));
-});
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "path-to-build-folder", "index.html"));
+// });
 
 import userRouter from "./src/router/user.route.js";
 import productRouter from "./src/router/product.route.js";
